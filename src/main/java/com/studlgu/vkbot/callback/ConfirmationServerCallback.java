6d8327257dd7ff9bd0270a32a5c8b44e103c6251
pomelo <ame1ou123455@gmail.com>
2026-02-21 01:53:00 +0300

    log enrichment 2

--- src/main/java/com/studlgu/vkbot/callback/ConfirmationServerCallback.java
+++ src/main/java/com/studlgu/vkbot/callback/ConfirmationServerCallback.java
@@ -1,6 +1,8 @@
 package com.studlgu.vkbot.callback;
 
 import com.studlgu.vkbot.model.ConfirmationServerRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,12 +17,18 @@ public class ConfirmationServerCallback {
     @Value("${vkbot.confirmation-code}")
     private String confirmationCode;
 
+    private static final Logger log = LoggerFactory.getLogger(ConfirmationServerCallback.class);
+
     @PostMapping("/callback")
     public String callback(@RequestBody ConfirmationServerRequest request) {
+        log.info("body type: " + request.getType());
+        log.info("body group: " + request.getGroupId());
         if (request.getType().contains("confirmation") && request.getGroupId().equals(Long.valueOf(groupId))) {
+            log.info("success");
             return confirmationCode;
         }
 
+        log.info("not success");
         return "callback was not recognized";
     }
 }
