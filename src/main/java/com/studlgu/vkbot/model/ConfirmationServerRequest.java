118af20424022f744bfa3799a0c15b6054eba437
pomelo <ame1ou123455@gmail.com>
2026-02-21 02:25:24 +0300

    fix property naming strategy

--- src/main/java/com/studlgu/vkbot/model/ConfirmationServerRequest.java
+++ src/main/java/com/studlgu/vkbot/model/ConfirmationServerRequest.java
@@ -2,9 +2,12 @@ package com.studlgu.vkbot.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import tools.jackson.databind.PropertyNamingStrategies;
+import tools.jackson.databind.annotation.JsonNaming;
 
 @Getter
 @Setter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ConfirmationServerRequest {
     private String type;
     private Long groupId;
